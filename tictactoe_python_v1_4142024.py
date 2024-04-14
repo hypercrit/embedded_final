c493d6c4fa29f99441580c79848d48c4b4ff13a9
hypercrit <165323953+hypercrit@users.noreply.github.com>
2024-04-14 16:40:12 -0400

    Add files via upload
    First Day of Work, Got a sample tic-tac-toe board printed. Working on turn state

---
+++ tictactoe_python_v1_4142024.py
@@ -0,0 +1,8 @@
+board = ['0','1','2','3','4','5','6','7','8','9']
+
+def initBoardState():
+    print("\n", board[1], board[2], board[3])
+    print("\n", board[4], board[5], board[6])
+    print("\n", board[7], board[8], board[9])
+
+initBoardState()
